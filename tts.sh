013855a7e991dcd799874a63b120d78be86c00e8
Anton Kobzev <r2dmd@mail.ru>
2026-01-29 23:52:30 +0300

    Pipeline script for TTS step

--- tts.sh
+++ tts.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
+# Script version date 2026-01-29
+export HAMAI_PROFILE=banan
+
 source ../piper-tts/piper/bin/activate
 
 while true; do
-	
-	for FILE in ./pipeline/tts/*; do
+	DIRPATH="./pipeline/tts/${HAMAI_PROFILE}_*"
+
+	for FILE in $DIRPATH; do
 	
 	  if [[ -f "$FILE" && $FILE != $0 ]]; then # Check if it's a regular file
-		echo "[TTS] File found: $FILE. Processing..."
 		filename=$(basename $FILE)
 		basename="${filename%.*}"
 		python3 -m piper -m ru_RU-dmitri-medium -i $FILE -f pipeline/tx/$basename.wav 
-		echo "[TTS] File added: pipeline/tx/$basename.wav"
+		echo "[TTS] $FILE => pipeline/tx/$basename.wav"
 		mv "$FILE" "processed/tts/$filename"
 	  fi
 	done
 	
-	sleep 0.5
+	sleep 0.1
 done
