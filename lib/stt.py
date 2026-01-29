cfff2eeda8417fd90543b477fd99d392f233ce99
Anton Kobzev <r2dmd@mail.ru>
2026-01-29 23:58:56 +0300

    Runner for Whisper AI

--- lib/stt.py
+++ lib/stt.py
@@ -1,55 +1,45 @@
-import os
-import shutil
+# Script version date 2026-01-28
 import time
 import whisper
+from hamai_functions import *
 
-def get_first_file_os(directory_path):
-    try:
-        files = [f for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))]
-        if files:
-            return os.path.join(directory_path, files[0])
-        else:
-            return None
-    except FileNotFoundError:
-        print(f"Error: Directory '{directory_path}' not found.")
-        return None
-
-def move_file(file_path):
-	destination_file = os.path.join("processed/stt", os.path.basename(file_path))
-	try:
-		shutil.move(file_path, destination_file)
-		#print(f"File '{file_path}' moved successfully to '{destination_file}'")
-	except FileNotFoundError:
-		print(f"Error: Source file '{file_path}' not found.")
-	except Exception as e:
-		print(f"An error occurred: {e}")
+profile_name = define_hamai_profile()
 
 model = whisper.load_model("turbo")
 
 while True:
-	first_file_path = get_first_file_os("pipeline/stt")
-	if first_file_path:
-		print(f"[STT] File found: {first_file_path}")
-		
+	stt_file_path = get_next_file_from_pipeline("pipeline/stt",profile_name)
+	if stt_file_path:
 		start_time = time.time()
-		
+		#print(f"[STT] File found: {stt_file_path}")
+				
 		# Whisper call
-		result = model.transcribe(first_file_path, language="ru")
+		result = model.transcribe(stt_file_path, language="ru")
 		
-		# Saving output to pipeline
-		filename_with_extension = os.path.basename(first_file_path)
+		# Check if anything valuable was decoded
+		result_text = result["text"]
+		result_text = result_text.strip()
+		if not result_text or result_text=="Продолжение следует...":
+			print(f"[STT] Empty file: {stt_file_path}")
+			move_processed_file(stt_file_path)
+			continue
+		
+		# Generate filename for GPT prompt file
+		filename_with_extension = os.path.basename(stt_file_path)
 		output_filename = os.path.splitext(filename_with_extension)[0]
 		output_filename += ".txt"
 		gpt_prompt_file_path = os.path.join("pipeline/gpt", output_filename)
-		with open(gpt_prompt_file_path, "w") as gpt_prompt_file:
-			gpt_prompt_file.write(result["text"])
 		
-		# Moving processed file
-		move_file(first_file_path)
+		# Save text to GPT prompt file
+		with open(gpt_prompt_file_path, "w") as gpt_prompt_file:
+			gpt_prompt_file.write(result_text)
+		
+		# Moving processed STT file
+		move_processed_file(stt_file_path)
 		
 		end_time = time.time()
 		execution_time = end_time - start_time
-		print(f"[STT] Processing time: {execution_time:.6f} seconds")
-		print(f"[STT] File added: {gpt_prompt_file_path}")
-
-	time.sleep(0.5)
+		#print(f"[STT] Processing time: {execution_time:.6f} seconds")
+		#print(f"[STT => GPT] File added: {gpt_prompt_file_path}")
+		print(f"[STT] {stt_file_path} => {gpt_prompt_file_path} in {execution_time:.1f} seconds")
+	time.sleep(0.1)
