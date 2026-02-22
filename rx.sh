4491adeadaa7acc3caf081a308e974ab358c27e8
Anton Kobzev <r2dmd@mail.ru>
2026-02-22 12:58:13 +0300

    RX & TX with profile support

--- rx.sh
+++ rx.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
+# Script version date 2026-01-29
 # To install dependecies run: sudo apt install sox libsox-fmt-all
 
-if [ "$#" -lt 1 ]; then
-    echo "Usage: $0 <audio_interface>"
-    echo "Example: $0 0,2"
-    echo "[Hint] To see available capture devices run: arecord -l" 
-	exit 1
-fi
+export HAMAI_PROFILE=banan
+
+export AUDIODRIVER=alsa
+export AUDIODEV=hw:0,0
+
+#if [ "$#" -lt 1 ]; then
+#	echo "Usage: $0 <audio_interface>"
+#   echo "Example: $0 0,2"
+#   echo "[Hint] To see available capture devices run: arecord -l" 
+#	exit 1
+#fi
 
 # Set ID here. Recommended format: QTH_FREQ. This ID is included in the filename for all generated files
-readonly Rxid="msk15_145425"
+#readonly Rxid="${HAMAI_PROFILE}"
 
 # HW device
-readonly DEVICE="$1"
-readonly HW="hw${DEVICE//,}"
+#readonly DEVICE="$1"
+#readonly HW="hw${DEVICE//,}"
 
 # Startup messages
-echo "[$Rxid] Listening audio interface $DEVICE"
+#echo "[$Rxid] Listening audio interface $DEVICE"
 
 # Main loop
 while true; do
 	timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
-	filename="${Rxid}_${HW}_${timestamp}.wav"
-	rec -t alsa hw:$1 -c 1 pipeline/rx/$filename silence 1 0.1 3% 1 0.5 3%
+	filename="${HAMAI_PROFILE}_${timestamp}.wav"
+	#filename="${Rxid}_${HW}_${timestamp}.wav"
+	#rec -t alsa hw:$1 -c 1 pipeline/rx/$filename silence 1 0.1 3% 1 0.5 3%
+	rec -c 1 pipeline/rx/$filename silence 1 0.1 3% 1 0.5 3%
 	mv pipeline/rx/$filename pipeline/stt/$filename	
 	echo "[RX] File added to pipeline: $filename"
 done
--- tx.sh
+++ tx.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
+# Script version date 2026-01-29
 # To install dependecies run: sudo apt install sox libsox-fmt-all
 
-if [ "$#" -lt 1 ]; then
-    echo "Usage: $0 <audio_interface>"
-    echo "Example: $0 0,2"
-    echo "[Hint] To see available capture devices run: arecord -l" 
-	exit 1
-fi
+export HAMAI_PROFILE=banan
+
+export AUDIODRIVER=alsa
+export AUDIODEV=hw:0,0
 
 stty -F /dev/ttyUSB0 -hupcl
 
 while true; do
-	
-	for FILE in ./pipeline/tx/*; do
+	DIRPATH="./pipeline/tx/${HAMAI_PROFILE}_*"
+
+	for FILE in $DIRPATH; do
 	
 	  if [[ -f "$FILE" && $FILE != $0 ]]; then # Check if it's a regular file
 		echo "[TX] File found: $FILE. Processing..."
 		
 		echo ptt_on > /dev/ttyUSB0
-		sleep 0.5
-		 
-		play -t alsa hw:$1 "$FILE" 
+		sleep 1
+		
+		play "$FILE" &&
+		#play -t alsa hw:$1 "$FILE" 
 		#sox "$FILE" -t alsa 0 &&
 		
 		echo ptt_off > /dev/ttyUSB0
@@ -31,5 +32,5 @@ while true; do
 	  
 	done
 	
-	sleep 0.5
+	sleep 0.1
 done
